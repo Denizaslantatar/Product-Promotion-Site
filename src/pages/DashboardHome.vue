a6512dcc0e4e9dd9f06262de948a1e51a1efe1d3
Denizaslantatar <100479163+Denizaslantatar@users.noreply.github.com>
2024-01-23 16:50:46 +0300

    new project created

---
+++ src/pages/DashboardHome.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    data() {
+        return {};
+    },
+    computed: {},
+    methods: {},
+    async mounted() {},
+};
+</script>
+<template>
+    <div>Test Component</div>
+</template>
