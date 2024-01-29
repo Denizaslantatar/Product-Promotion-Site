13e4f0e4de37284cf7b8a258fed2e8c8bf43dca5
Denizaslantatar <100479163+Denizaslantatar@users.noreply.github.com>
2024-01-29 16:16:54 +0300

    layout fixed

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -10,13 +10,13 @@ export default {
 </script>
 
 <template>
-    <footer class="bg-success text-center text-lg-start text-white fixed-bottom" style="max-height: 400px; overflow-y: auto">
+    <footer class="bg-success text-center text-lg-start text-white fixed-bottom">
         <!-- Grid container -->
-        <div class="container p-3">
-            <div class="row my-3">
+        <div class="container p-2">
+            <div class="row my-2">
                 <!--Grid column-->
-                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                    <div class="rounded-circle bg-white shadow-1-strong d-flex align-items-center justify-content-center mb-4 mx-auto" style="width: 100px; height: 100px">
+                <div class="col-lg-3 col-md-6 mb-md-0 mt-1">
+                    <div class="rounded-circle bg-white shadow-1-strong d-flex align-items-center justify-content-center mb-1 mx-auto" style="width: 100px; height: 100px">
                         <img src="../assets/logo.png" height="70" alt="" loading="lazy" />
                     </div>
                     <p class="text-center">Cami Çinicilik Hizmetleri</p>
@@ -36,83 +36,83 @@ export default {
                     </ul>
                 </div>
                 <!--Grid column-->
-                <div class="col-lg-3 col-md-6 mb-2 mb-md-0">
-                    <h5 class="text-uppercase mb-2">Hizmetlerimiz</h5>
+                <div class="col-lg-3 col-md-6 mb-1 mb-md-0 mt-1">
+                    <h5 class="text-uppercase mb-1">Hizmetlerimiz</h5>
                     <ul class="list-unstyled">
-                        <li class="mb-2">
+                        <li class="mb-1">
                             <span class="text-white">Cami çinicilik</span>
                         </li>
-                        <li class="mb-2">
+                        <li class="mb-1">
                             <span class="text-white">Otel çinicilik</span>
                         </li>
-                        <li class="mb-2">
+                        <li class="mb-1">
                             <span class="text-white">Hamam Çinicilik</span>
                         </li>
-                        <li class="mb-2">
+                        <li class="mb-1">
                             <span class="text-white">Şadırvan Çinicilik</span>
                         </li>
-                        <li class="mb-2">
+                        <li class="mb-1">
                             <span class="text-white">Villa çinicilik</span>
                         </li>
-                        <li class="mb-2">
+                        <li class="mb-1">
                             <span class="text-white">Nakkaş</span>
                         </li>
-                        <li class="mb-2">
+                        <li class="mb-1">
                             <span class="text-white">Altın Varak</span>
                         </li>
-                        <li class="mb-2">
+                        <li class="mb-1">
                             <span class="text-white">Hat Yazıları</span>
                         </li>
                     </ul>
                 </div>
                 <!--Grid column-->
-                <div class="col-lg-3 col-md-6 mb-2 mb-md-0">
-                    <h5 class="text-uppercase mb-2">Referanslarımız</h5>
+                <div class="col-lg-3 col-md-6 mb-1 mb-md-0 mt-1">
+                    <h5 class="text-uppercase mb-1">Referanslarımız</h5>
                     <ul class="list-unstyled">
-                        <li class="mb-2">
+                        <li class="mb-1">
                             <span class="text-white">Gaziantep </span>
                         </li>
-                        <li class="mb-2">
+                        <li class="mb-1">
                             <span class="text-white">Erzurum</span>
                         </li>
-                        <li class="mb-2">
+                        <li class="mb-1">
                             <span class="text-white">İstanbul</span>
                         </li>
-                        <li class="mb-2">
+                        <li class="mb-1">
                             <span class="text-white">Afyon</span>
                         </li>
-                        <li class="mb-2">
+                        <li class="mb-1">
                             <span class="text-white">Kocaeli</span>
                         </li>
-                        <li class="mb-2">
+                        <li class="mb-1">
                             <span class="text-white">Tekirdağ</span>
                         </li>
-                        <li class="mb-2">
+                        <li class="mb-1">
                             <span class="text-white">Denizli</span>
                         </li>
                     </ul>
                 </div>
                 <!--Grid column-->
-                <div class="col-lg-3 col-md-6 mb-2">
-                    <h5 class="text-bold mb-2">İLETİŞİM</h5>
+                <div class="col-lg-3 col-md-6 mb-1 mt-1">
+                    <h5 class="text-bold mb-1">İLETİŞİM</h5>
                     <ul class="list-unstyled">
                         <li>
-                            <p><img src="../assets/icons/icons8-address-25.png" alt="telefon" /> Bingöl / Merkez</p>
+                            <p class="mb-2"><img src="../assets/icons/icons8-address-25.png" alt="telefon" /> Bingöl / Merkez</p>
                         </li>
                         <li>
-                            <p><img src="../assets/icons/icons8-phone.svg" alt="telefon" /> +90 552 807 60 89</p>
+                            <p class="mb-2"><img src="../assets/icons/icons8-phone.svg" alt="telefon" /> +90 552 807 60 89</p>
                         </li>
                         <li>
-                            <p><img src="../assets/icons/icons8-letter-25.png" alt="telefon" /> camicinicilik@gmail.com</p>
+                            <p class="mb-2"><img src="../assets/icons/icons8-letter-25.png" alt="telefon" /> camicinicilik@gmail.com</p>
                         </li>
                     </ul>
                 </div>
             </div>
         </div>
         <!-- Grid container -->
-        <div class="text-center p-3" style="background-color: rgba(1, 15, 5, 0.2)">
+        <div class="text-center p-2" style="background-color: rgba(1, 15, 5, 0.2)">
             © 2020 Cami Çinicilik:
-            <span class="text-white">Tüm hakları saklıdır.</span>
+            <span class="text-white" style="font-style: italic">Tüm hakları saklıdır.</span>
         </div>
     </footer>
 </template>
