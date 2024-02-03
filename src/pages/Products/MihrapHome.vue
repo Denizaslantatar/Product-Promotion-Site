cb6ca160b2fb9ab1c6d0c30d0c7827ed7e91ea7b
Denizaslantatar <100479163+Denizaslantatar@users.noreply.github.com>
2024-02-03 12:58:40 +0300

    dynamic structure created

--- src/pages/Products/MihrapHome.vue
+++ src/pages/Products/MihrapHome.vue
@@ -16,77 +16,16 @@ export default {
         <h5 class="text-center p-2">Mihrap</h5>
     </div>
     <section id="mihrapCard">
-        <div class="row row-cols-1 row-cols-md-3 g-4" v-for="item in MihrapData?.Mihrap" :key="item">
-            <div class="col">
+        <div class="row row-cols-lg-3 row-cols-md-3 row-cols-1 g-4">
+            <div class="col" v-for="item in MihrapData?.Mihrap" :key="item">
                 <div class="card h-100">
-                    <img :src="'@/assets/images/Mihrap/' + item.imageUrl" class="card-img-top" alt="..." />
-                    {{ item.imageUrl }}
+                    <img :src="'/images/Mihrap/' + item.imageUrl" class="card-img-top" alt="..." />
                     <div class="card-body">
                         <h5 class="card-title">{{ item.title }}</h5>
                         <p class="card-text text-danger fw-bold">{{ item.text }}</p>
                     </div>
                     <div class="card-footer">
                         <small class="text-body-secondary">{{ item.footer }}</small>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="../../assets/images/Mihrap/2-180x290-cm.jpeg" class="card-img-top" alt="..." />
-                    <div class="card-body">
-                        <h5 class="card-title">Zamanın İzleri: Antik Desenli Mihrap</h5>
-                        <p class="card-text text-danger fw-bold">180x290 cm</p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-body-secondary">Mihrap 2</small>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="../../assets/images/Mihrap/3-220x370-cm.jpeg" class="card-img-top" alt="..." />
-                    <div class="card-body">
-                        <h5 class="card-title">Renkli Doku: Zarif Mavi Mihrap</h5>
-                        <p class="card-text text-danger fw-bold">220x370 cm</p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-body-secondary">Mihrap 3</small>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="../../assets/images/Mihrap/4-230x400-cm.jpeg" class="card-img-top" alt="..." />
-                    <div class="card-body">
-                        <h5 class="card-title">Geçmişten Günümüze: Modern Çini Mihrap</h5>
-                        <p class="card-text text-danger fw-bold">230x400 cm</p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-body-secondary">Mihrap 4</small>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="../../assets/images/Mihrap/5-240x400-cm.jpeg" class="card-img-top" alt="..." />
-                    <div class="card-body">
-                        <h5 class="card-title">Deniz Rüzgarları: Deniz Mavisi Mihrap</h5>
-                        <p class="card-text text-danger fw-bold">240x400 cm</p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-body-secondary">Mihrap 5</small>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card h-100">
-                    <img src="../../assets/images/Mihrap/6-240x400-cm.jpeg" class="card-img-top" alt="..." />
-                    <div class="card-body">
-                        <h5 class="card-title">Gök kubbe: Gökyüzü Mihrap</h5>
-                        <p class="card-text text-danger fw-bold">240x400 cm</p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-body-secondary">Mihrap 6</small>
                     </div>
                 </div>
             </div>
