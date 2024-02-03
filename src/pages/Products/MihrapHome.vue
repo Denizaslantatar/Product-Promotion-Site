5ea1000059aef8909d62f4eaaf6769517a2f487d
Denizaslantatar <100479163+Denizaslantatar@users.noreply.github.com>
2024-02-03 11:50:11 +0300

    product page updated

--- src/pages/Products/MihrapHome.vue
+++ src/pages/Products/MihrapHome.vue
@@ -12,4 +12,80 @@ export default {
     <div class="header">
         <h5 class="text-center p-2">Mihrap</h5>
     </div>
+    <section id="mihrapCard">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card h-100">
+                    <img src="../../assets/images/Mihrap/1-180x290-cm.jpeg" class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <h5 class="card-title">Geleneksel Zarafet: İlk Mihrap</h5>
+                        <p class="card-text text-danger fw-bold">180x290 cm</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-body-secondary">Mihrap 1</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="../../assets/images/Mihrap/2-180x290-cm.jpeg" class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <h5 class="card-title">Zamanın İzleri: Antik Desenli Mihrap</h5>
+                        <p class="card-text text-danger fw-bold">180x290 cm</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-body-secondary">Mihrap 2</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="../../assets/images/Mihrap/3-220x370-cm.jpeg" class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <h5 class="card-title">Renkli Doku: Zarif Mavi Mihrap</h5>
+                        <p class="card-text text-danger fw-bold">220x370 cm</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-body-secondary">Mihrap 3</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="../../assets/images/Mihrap/4-230x400-cm.jpeg" class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <h5 class="card-title">Geçmişten Günümüze: Modern Çini Mihrap</h5>
+                        <p class="card-text text-danger fw-bold">230x400 cm</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-body-secondary">Mihrap 4</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="../../assets/images/Mihrap/5-240x400-cm.jpeg" class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <h5 class="card-title">Deniz Rüzgarları: Deniz Mavisi Mihrap</h5>
+                        <p class="card-text text-danger fw-bold">240x400 cm</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-body-secondary">Mihrap 5</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="../../assets/images/Mihrap/6-240x400-cm.jpeg" class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <h5 class="card-title">Gök kubbe: Gökyüzü Mihrap</h5>
+                        <p class="card-text text-danger fw-bold">240x400 cm</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-body-secondary">Mihrap 6</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
