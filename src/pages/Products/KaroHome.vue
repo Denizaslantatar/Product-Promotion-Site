f3bed0f06e9e3cf5620b373d7ea9453904bd60f6
Denizaslantatar <100479163+Denizaslantatar@users.noreply.github.com>
2024-02-14 12:47:34 +0300

    karo page updated

--- src/pages/Products/KaroHome.vue
+++ src/pages/Products/KaroHome.vue
@@ -1,30 +1,27 @@
 <script>
+import KaroData from "@/JSON/karoData.json";
 export default {
     data() {
-        return {};
+        return {
+            KaroData,
+        };
     },
-    computed: {},
-    methods: {},
-    async mounted() {},
 };
 </script>
 <template>
-    <div class="row row-cols-1 row-cols-md-3 g-4">
-        <div class="col">
-            <div class="card">
-                <img src="../../../public/images/Karolar/iyk01-1.jpg" class="card-img-top" alt="..." />
-                <div class="card-body">
-                    <h5 class="card-title">Karo 1</h5>
-                </div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="card">
-                <img src="../../../public/images/Karolar/iyk01-1.jpg" class="card-img-top" alt="..." />
-                <div class="card-body">
-                    <h5 class="card-title">Karo 2</h5>
-                </div>
-            </div>
-        </div>
+    <div class="header">
+        <h5 class="text-center p-2">Karolar</h5>
     </div>
+    <section id="karoCard">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col" v-for="item in KaroData?.Karo" :key="item">
+                <div class="card">
+                    <img :src="'/images/Karolar/' + item.imageUrl" class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <h5 class="card-title">{{ item.title }}</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
