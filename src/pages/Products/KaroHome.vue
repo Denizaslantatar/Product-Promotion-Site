79ffa2c7f49b4ac6e0b01425625290f2bf471621
Denizaslantatar <100479163+Denizaslantatar@users.noreply.github.com>
2024-02-14 12:26:58 +0300

    updated

--- src/pages/Products/KaroHome.vue
+++ src/pages/Products/KaroHome.vue
@@ -9,15 +9,22 @@ export default {
 };
 </script>
 <template>
-    <div class="card-group">
-        <div class="card">
-            <img src="../../../public/images/Karolar/iyk01-1.jpg" class="card-img" alt="..." style="width: 18rem" />
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        <div class="col">
+            <div class="card">
+                <img src="../../../public/images/Karolar/iyk01-1.jpg" class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">Karo 1</h5>
+                </div>
+            </div>
         </div>
-        <div class="card">
-            <img src="../../../public/images/Karolar/iyk01-1.jpg" class="card-img" alt="..." style="width: 18rem" />
-        </div>
-        <div class="card">
-            <img src="../../../public/images/Karolar/iyk01-1.jpg" class="card-img" alt="..." style="width: 18rem" />
+        <div class="col">
+            <div class="card">
+                <img src="../../../public/images/Karolar/iyk01-1.jpg" class="card-img-top" alt="..." />
+                <div class="card-body">
+                    <h5 class="card-title">Karo 2</h5>
+                </div>
+            </div>
         </div>
     </div>
 </template>
