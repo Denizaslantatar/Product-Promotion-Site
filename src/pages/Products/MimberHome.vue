d8d4bf111a755b0853a541bec8799660280a5a5a
Denizaslantatar <100479163+Denizaslantatar@users.noreply.github.com>
2024-02-17 17:55:53 +0300

    mimber page created

--- src/pages/Products/MimberHome.vue
+++ src/pages/Products/MimberHome.vue
@@ -1,7 +1,10 @@
 <script>
+import MimberData from "@/JSON/mimberData.json";
 export default {
     data() {
-        return {};
+        return {
+            MimberData,
+        };
     },
     computed: {},
     methods: {},
@@ -9,5 +12,19 @@ export default {
 };
 </script>
 <template>
-    <div>Test Component</div>
+    <div class="header">
+        <h5 class="text-center p-2">Mimber</h5>
+    </div>
+    <section id="mimberCard">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col" v-for="item in MimberData?.Mimber" :key="item">
+                <div class="card">
+                    <img :src="'/images/Mimber/' + item.imageUrl" class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <h5 class="card-title">{{ item.title }}</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
