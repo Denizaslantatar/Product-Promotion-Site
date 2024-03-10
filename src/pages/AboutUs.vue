a3f7a62aa1486bf964c6fbd1e626bbc6c8f8ec53
Denizaslantatar <100479163+Denizaslantatar@users.noreply.github.com>
2024-03-10 19:31:56 +0300

    about us page updated

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -51,17 +51,17 @@ export default {
         </section>
         <section id="hakkimizdaText">
             <div class="container">
-                <p>
-                    <span class="text-center fw-700">Merhabalar,</span>
-                    ini sanatının geleneksel dokusunu modern mimariyle buluşturan bir atölyeyiz. Yıllar boyu edindiğimiz deneyim ve tutkuyla, cami mimberleri, mihraplar, kürsüler ve otel duvarlarından
-                    hamam ve şadırvanlara kadar geniş bir yelpazede çini işleri üretiyoruz. Çini ustalarımızın ellerinde, her parça bir sanat eserine dönüşürken, yapılan her işte geleneksel
-                    tekniklerle çağdaş tasarımları harmanlıyoruz. Türkiye'nin dört bir yanında, pek çok caminin iç dizaynını gerçekleştirerek adımızı çini sanatında kalıcı kılmış bulunmaktayız. Cami
-                    duvarlarına yerleştirdiğimiz çini işleri, ibadetin huzurunu ve estetiğini yansıtmak üzere özenle seçilmiş motiflerle bezenmiştir. Otellerin duvarlarında ve koridorlarında ise çini
-                    karolarımız, konuklara sıcak bir atmosfer sunmanın yanı sıra, Türk kültürünün zenginliğini ve zarafetini yansıtmaktadır. Hamam ve şadırvanlarda dizayn ettiğimiz çini işleri,
-                    geleneksel Türk hamam kültürünün modern yorumunu sunar. Her bir detay, ziyaretçilere tarihin derinliklerinde bir yolculuk vadederek, ruhu ve bedeni dinlendirme amacı taşır. Bizim
-                    için çini sadece bir iş değil, bir tutkudur. Her bir projemizde, geçmişten gelen mirası gelecek nesillere aktarmak ve yaşatmak adına çalışıyoruz. Müşterilerimizin taleplerini
-                    özenle dinleyip, onların hayallerini gerçeğe dönüştürmek için var gücümüzle çalışıyoruz. Siz de bize katılın, yaşam alanlarınızı çiniyle buluşturun ve Türk sanatının eşsiz
-                    güzelliğini evinize, iş yerinize veya ibadet mekanlarınıza taşıyın. Biz, sizin için buradayız.
+                <h5 class="text-center fw-bold">Biz kimiz</h5>
+                <p class="mt-2 mb-5">
+                    Çini sanatının geleneksel dokusunu modern mimariyle buluşturuyoruz. Yıllar boyu edindiğimiz deneyim ve tutkuyla, cami mimberleri, mihraplar, kürsüler ve otel duvarlarından hamam ve
+                    şadırvanlara kadar geniş bir yelpazede çini karolar ile çalışmalar yapıyoruz. Elimizde her parça bir sanat eserine dönüşürken, yapılan her işte geleneksel tekniklerle çağdaş
+                    tasarımları harmanlıyoruz. Türkiye'nin dört bir yanında, pek çok caminin iç dizaynını gerçekleştirerek adımızı çini sanatında kalıcı kılmış bulunmaktayız. Cami duvarlarına
+                    yerleştirdiğimiz çini işleri, ibadetin huzurunu ve estetiğini yansıtmak üzere özenle seçilmiş motiflerle bezenmiştir. Otellerin duvarlarında ve koridorlarında ise çini karolarımız,
+                    konuklara sıcak bir atmosfer sunmanın yanı sıra, Türk kültürünün zenginliğini ve zarafetini yansıtmaktadır. Hamam ve şadırvanlarda dizayn ettiğimiz çini işleri, geleneksel Türk
+                    hamam kültürünün modern yorumunu sunar. Her bir detay, ziyaretçilere tarihin derinliklerinde bir yolculuk vadederek, ruhu ve bedeni dinlendirme amacı taşır. Bizim için çini sadece
+                    bir iş değil, bir tutkudur. Her bir projemizde, geçmişten gelen mirası gelecek nesillere aktarmak ve yaşatmak adına çalışıyoruz. Müşterilerimizin taleplerini özenle dinleyip,
+                    onların hayallerini gerçeğe dönüştürmek için var gücümüzle çalışıyoruz. Siz de bize katılın, yaşam alanlarınızı çiniyle buluşturun ve Türk sanatının eşsiz güzelliğini evinize, iş
+                    yerinize veya ibadet mekanlarınıza taşıyın. Biz, sizin için buradayız.
                 </p>
             </div>
         </section>
