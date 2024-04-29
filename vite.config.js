3c57ae16ac42f38ad2ea51cce90efb84b7bc3ec9
Denizaslantatar <100479163+Denizaslantatar@users.noreply.github.com>
2024-04-29 14:26:04 +0300

    htacc added

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,49 @@
-import { fileURLToPath, URL } from 'node:url';
+import {fileURLToPath, URL} from "node:url";
 import viteCompression from "vite-plugin-compression";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import {defineConfig} from "vite";
+import vue from "@vitejs/plugin-vue";
+import fs from "fs";
+import path from "path";
+/* eslint-disable */
+const GenerateAndCopyHtaccess = async () => {
+    console.log("Generating .htaccess");
+    const htaccess = `RewriteEngine On
+RewriteBase /
+RewriteRule ^index\.html$ - [L]
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule . /index.html [L]`;
+    fs.writeFileSync(path.join(__dirname, "dist", ".htaccess"), htaccess);
+    console.log("Copied .htaccess to dist");
+};
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    viteCompression(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    port: 1212,
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        charset: false,
-      },
+const GenerateHtaccess = async () => {
+    return {
+        name: "after-build",
+        apply: "build",
+        async closeBundle() {
+            await GenerateAndCopyHtaccess();
+        },
+    };
+};
+export default defineConfig(async () => ({
+    plugins: [vue(), viteCompression(), await GenerateHtaccess()],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
     },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                charset: false,
+            },
+        },
         devSourcemap: true,
-      },
-})
+    },
+
+    server: {
+        port: 1212,
+    },
+}));
